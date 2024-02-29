4c12d4a39024b2aeaa263d1f8b227b0e3598f0ae
vidhi-7span <vidhi@7span.com>
2024-02-29 14:40:50 +0530

    Update Functionality

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <p class="text-center w-full my-6 text-2xl font-bold underline text-gray-700">
-    Basic Accordion
-  </p>
-  <accordion />
-  <p class="text-center w-full my-6 text-2xl font-bold underline text-gray-700">
-    Accordion with Close Option
-  </p>
-  <accordionOption />
+  <div class="flex items-center justify-center my-20">
+    <div class="space-y-2 w-full max-w-5xl">
+      <div v-for="(detail, i) in Details" :key="i">
+        <accordion :title="detail.title" :content="detail.content" />
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
+import { ref } from "vue";
 import accordion from "./components/accordion.vue";
-import accordionOption from "./components/accordionOption.vue";
+
+const Details = ref([
+  {
+    title: "Title",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Title",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Title",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+]);
 </script>
--- src/components/accordion.vue
+++ src/components/accordion.vue
@@ -1,25 +1,24 @@
 <template>
-  <div class="container w-full mx-auto">
-    <div v-for="(item, index) in items" :key="index">
-      <div
-        @click="toggle(index)"
-        class="flex items-center justify-between p-4 cursor-pointer border-b border-gray-400 bg-gray-100 mb-2"
+  <div class="w-full border">
+    <button
+      @click="toggleAcc"
+      class="flex items-center w-full justify-between p-4 cursor-pointer border-gray-400 bg-gray-100"
+    >
+      <div class="flex items-center container">
+        <span class="font-semibold">{{ title }}</span>
+      </div>
+      <svg
+        class="h-6 w-6 transition-all duration-300"
+        :class="isAccOpen ? 'rotate-180' : ''"
       >
-        <div class="flex items-center container">
-          <span class="font-semibold">{{ item.title }}</span>
-        </div>
-        <svg
-          class="h-6 w-6 transition-transform"
-          :class="{ rotate: Index === index }"
-        >
-          <path
-            d="M10 18a.75.75 0 01-.53-.22l-6.25-6.25a.75.75 0 111.06-1.06L10 16.94l5.72-5.72a.75.75 0 111.06 1.06l-6.25 6.25a.75.75 0 01-.53.22z"
-          />
-        </svg>
-      </div>
-      <div v-if="Index === index" class="p-4">
-        <p>{{ item.content }}</p>
-      </div>
+        <path
+          d="M10 18a.75.75 0 01-.53-.22l-6.25-6.25a.75.75 0 111.06-1.06L10 16.94l5.72-5.72a.75.75 0 111.06 1.06l-6.25 6.25a.75.75 0 01-.53.22z"
+        />
+      </svg>
+    </button>
+    <div v-if="isAccOpen" class="p-4">
+      <p v-if="content">{{ content }}</p>
+      <div v-else><slot name="content"></slot></div>
     </div>
   </div>
 </template>
@@ -27,33 +26,25 @@
 <script setup>
 import { ref } from "vue";
 
-const items = ref([
-  {
-    title: "Title",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+const emit = defineEmits(["toggled"]);
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    default: false,
   },
-  {
-    title: "Title",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  title: {
+    type: String,
+    default: "Title",
   },
-  {
-    title: "Title",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  content: {
+    type: String,
   },
-]);
+});
 
-let Index = ref(null);
+let isAccOpen = ref(props.isOpen);
 
-const toggle = (index) => {
-  Index.value = Index.value === index ? null : index;
+const toggleAcc = () => {
+  emit("toggled", isAccOpen.value);
+  isAccOpen.value = !isAccOpen.value;
 };
 </script>
-
-<style>
-.rotate {
-  transform: rotate(180deg);
-}
-</style>
